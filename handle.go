db92354ef24f8e3d9794e0f103e3e311659a2125
salaboy <Salaboy@gmail.com>
2022-04-17 15:58:38 +0100

    updating env vars and port

--- handle.go
+++ handle.go
@@ -30,14 +30,15 @@ type GameTime struct{
 	Time      time.Time
 }
 
-var redisHost = os.Getenv("REDIS_HOST")
+var redisHost = os.Getenv("REDIS_HOST")// This should include the port which is most of the time 6379
+var redisPassword = os.Getenv("REDIS_PASSWORD")
 
 // Handle an HTTP Request.
 func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 
 	client := redis.NewClient(&redis.Options{
-		Addr:     redisHost + ":6379",
-		Password: "",
+		Addr:     redisHost,
+		Password: redisPassword,
 		DB:       0,
 	})
 
